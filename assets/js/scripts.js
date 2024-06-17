68e830d00f3bbadcf9d8b5ad6fe3a2522f3231e3
Gustavo <envolti.gustavo.peron@redeunifique.com.br>
2024-06-17 16:00:23 -0300

    Ajuste datapact

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,109 +1,108 @@
-
-
 // Pega os atributos da URL
 function getURLParams() {
-    var url = document.location.href;
-    var paramsString = url.split('?')[1];
-    var data = {};
+  var url = document.location.href;
+  var paramsString = url.split("?")[1];
+  var data = {};
 
-    if (paramsString) {
-        var params = paramsString.split('&');
-        var tmp;
+  if (paramsString) {
+    var params = paramsString.split("&");
+    var tmp;
 
-        for (var i = 0, l = params.length; i < l; i++) {
-            tmp = params[i].split('=');
-            data[tmp[0]] = decodeURIComponent(tmp[1]);  // Decode the parameter value
-        }
-    } else {
-        console.warn('No query parameters found in the URL');
+    for (var i = 0, l = params.length; i < l; i++) {
+      tmp = params[i].split("=");
+      data[tmp[0]] = decodeURIComponent(tmp[1]); // Decode the parameter value
     }
+  } else {
+    console.warn("No query parameters found in the URL");
+  }
 
-    return data;
+  return data;
 }
-
 
 // Salve o theme
 function setThemeFromURL() {
-    var params = getURLParams();
-    if (params.theme) {
-        localStorage.setItem('theme', params.theme);
-    }
+  var params = getURLParams();
+  if (params.theme) {
+    localStorage.setItem("theme", params.theme);
+  }
 
-    applyTheme();
+  applyTheme();
 }
 
 // Seta o theme
 function applyTheme() {
-    var theme = localStorage.getItem('theme');
-    if (theme) {
-        document.body.className = theme;
-        if(theme.includes('bradesco') || theme.includes('crefaz')){
-            $('.logo-datapact').addClass('d-none');
-            $(`.logo-${theme}`).addClass('d-block');
-        }
+  var theme = localStorage.getItem("theme");
+  if (theme) {
+    document.body.className = theme;
+    if (theme.includes("bradesco") || theme.includes("crefaz")) {
+      $(".logo-datapact").addClass("d-none");
+      $(`.logo-${theme}`).addClass("d-block");
     }
+  }
 }
 
+$(document).ready(function () {
+  // seta o theme
+  setThemeFromURL();
 
+  $(".link-disabled").on("click", function (event) {
+    event.preventDefault();
+    toastr["info"]("Ação desativada na versão demo.");
+  });
 
+  $(".ff-details").on("click", function (event) {
+    var myModal = new bootstrap.Modal(
+      document.getElementById("modal-detalhes")
+    );
+    myModal.show();
+  });
 
-$(document).ready(function() {
+  $("#avatar-click").on("click", function (event) {
+    var myModal = new bootstrap.Modal(document.getElementById("modal-no-user"));
+    myModal.show();
+  });
 
-    // seta o theme
-    setThemeFromURL();
+  $("#slider-contratos").owlCarousel({
+    stagePadding: 15,
+    loop: false,
+    margin: 10,
+    dots: false,
+    nav: false,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 3,
+      },
+      1000: {
+        items: 3,
+      },
+    },
+  });
 
-    $('.link-disabled').on('click', function(event) {
-        event.preventDefault(); 
-        toastr["info"]("Ação desativada na versão demo.");
+  //Coloca first-active e last-active no carousel
+  $(".owl-carousel").trigger("to.owl.carousel", 0);
+  function brandSliderClasses() {
+    $(".owl-carousel").each(function () {
+      var total = $(this).find(".owl-item.active").length;
+      $(this).find(".owl-item").removeClass("first-active");
+      $(this).find(".owl-item").removeClass("last-active");
+      $(this)
+        .find(".owl-item.active")
+        .each(function (index) {
+          if (index === 0) {
+            $(this).addClass("first-active");
+          }
+          if (index === total - 1 && total > 1) {
+            $(this).addClass("last-active");
+          }
+        });
     });
-
-    $('.ff-details').on('click', function(event) {
-        var myModal = new bootstrap.Modal(document.getElementById('modal-detalhes'));
-        myModal.show();
-        var myModal = new bootstrap.Modal(document.getElementById('modal-no-user'));
-        myModal.show();
-    });
-
-    $('#slider-contratos').owlCarousel({
-        stagePadding: 15,
-        loop:false,
-        margin:10,
-        dots:false,
-        nav:false,
-        responsive:{
-            0:{
-                items:1
-            },
-            600:{
-                items:3
-            },
-            1000:{
-                items:3
-            }
-        }
-    })
-
-    //Coloca first-active e last-active no carousel
-    $('.owl-carousel').trigger('to.owl.carousel', 0);
-    function brandSliderClasses() {
-        $('.owl-carousel').each(function() {
-            var total = $(this).find('.owl-item.active').length;
-            $(this).find('.owl-item').removeClass('first-active');
-            $(this).find('.owl-item').removeClass('last-active');
-            $(this).find('.owl-item.active').each(function(index) {
-                if (index === 0) {
-                    $(this).addClass('first-active')
-                }
-                if (index === total - 1 && total > 1) {
-                    $(this).addClass('last-active')
-                }
-            })
-        })
-    }
+  }
+  brandSliderClasses();
+  $(".owl-carousel").on("translated.owl.carousel", function (event) {
     brandSliderClasses();
-    $('.owl-carousel').on('translated.owl.carousel', function(event) {
-        brandSliderClasses()
-    });
-    $('.first-active .card-radio-label').click();
-
+  });
+  $(".first-active .card-radio-label").click();
 });
